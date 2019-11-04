99efdc4e2505d34389ada5e65eb570da1fa77064
Weber Amaral <weberamaral@outlook.com>
2019-11-04 21:41:05 -0200

    add nginx container

--- sidecard-https-to-legacy/node/index.js
+++ sidecard-https-to-legacy/node/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 
-const NODE_PORT = process.env.NODE_APP_PORT || 8080
-const NODE_HOST = process.env.NODE_APP_HOST || '0.0.0.0'
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World weber!')
 })
 
-app.listen(NODE_PORT, NODE_HOST)
-console.log(`Running on http://${NODE_HOST}:${NODE_PORT}`)
+app.listen(PORT)
+console.log(`Running on port ${PORT}`)
